a97293612bac00a9e38842080a11370f7a0c1ae2
Adityagautam50009 <126130456+Adityagautam50009@users.noreply.github.com>
2024-12-03 18:55:28 +0530

    Add files via upload

---
+++ src/pages/Coin/Coin.jsx
@@ -0,0 +1,115 @@
+import React, { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { CoinContext } from "../../context/CoinContext";
+import "./Coin.css";
+import LineChart from "../../components/LineChart/LineChart";
+
+const Coin = () => {
+  const { coinId } = useParams();
+  const [coinData, setCoinData] = useState();
+  const { currency } = useContext(CoinContext);
+  const [historicalData, setHistoricalData] = useState();
+  const apiKey = import.meta.env.VITE_APP_API_KEY
+
+  const fetchCoinData = async () => {
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        "x-cg-demo-api-key": apiKey,
+      },
+    };
+
+    fetch(`https://api.coingecko.com/api/v3/coins/${coinId}`, options)
+      .then((res) => res.json())
+      .then((res) => setCoinData(res))
+      .catch((err) => console.error(err));
+  };
+
+  const fetchHistoricalData = async () => {
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        "x-cg-demo-api-key": apiKey,
+      },
+    };
+
+    fetch(
+      `https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=${currency.name}&days=10&interval=daily`,
+      options
+    )
+      .then((res) => res.json())
+      .then((res) => setHistoricalData(res))
+      .catch((err) => console.error(err));
+  };
+
+  useEffect(() => {
+    fetchCoinData();
+    fetchHistoricalData();
+  }, [currency]);
+
+  console.log(historicalData);
+
+  if (coinData && historicalData) {
+    return (
+      <div className="coin">
+        <div className="coin-name">
+          <img src={coinData.image.large} alt="" />
+          <p>
+            <b>
+              {coinData.name} ({coinData.symbol.toUpperCase()})
+            </b>
+          </p>
+        </div>
+        <div className="coin-chart">
+          <LineChart historicalData={historicalData} />
+        </div>
+        <div className="coin-info">
+          <ul>
+            <li>Crypto market rank</li>
+            <li>{coinData.market_cap_rank}</li>
+          </ul>
+          <ul>
+            <li>Current Price</li>
+            <li>
+              {currency.symbol}
+              {coinData.market_data.current_price[
+                currency.name
+              ].toLocaleString()}
+            </li>
+          </ul>
+          <ul>
+            <li>Market Cap</li>
+            <li>
+              {currency.symbol}
+              {coinData.market_data.market_cap[currency.name].toLocaleString()}
+            </li>
+          </ul>
+          <ul>
+            <li>24 Hour High</li>
+            <li>
+              {currency.symbol}
+              {coinData.market_data.high_24h[currency.name].toLocaleString()}
+            </li>
+          </ul>
+          <ul>
+            <li>24 Hour Low</li>
+            <li>
+              {currency.symbol}
+              {coinData.market_data.low_24h[currency.name].toLocaleString()}
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="spinner">
+        <div className="spin"></div>
+      </div>
+    );
+  }
+};
+
+export default Coin;
